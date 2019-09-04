f361094cf93675fef92e38adc56f42320502dc85
Arthur Arend <arthur.nox@gmail.com>
2019-09-04 17:24:08 -0300

    Teste de alteração de setState

--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -16,10 +16,6 @@ export default class FilmList extends React.Component {
     render() {
         return(
             <div id="layout">
-                 <a href="#menu" id="menuLink" class="menu-link">
-                    <span></span>
-                </a>
-
                 <div id="menu">
                     <div class="pure-menu">
                         <a class="pure-menu-heading" href="#">Star Wars Wiki</a>
