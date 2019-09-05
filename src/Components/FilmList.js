12b4d3f36b475502ae4e04c8bdd82aa0a0d31b52
Arthur Arend <arthur.nox@gmail.com>
2019-09-05 11:47:39 -0300

    Correção do elemento class por ClassName

--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -18,26 +18,26 @@ export default class FilmList extends React.Component {
         return(
             <div id="layout">
                 <div id="menu">
-                    <div class="pure-menu">
-                        <a class="pure-menu-heading" href="#">Wars Wiki</a>
+                    <div className="pure-menu">
+                        <a className="pure-menu-heading" href="#">Wiki Wars</a>
 
-                        <ul class="pure-menu-list">
-                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">opening_crawl </a></li>
-                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">director </a></li>
-                            <li class="pure-menu-item"><a href="#" value="release_date" class="pure-menu-link">release_date </a></li>
+                        <ul className="pure-menu-list">
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">opening_crawl </a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">director </a></li>
+                            <li className="pure-menu-item"><a href="#" value="release_date" className="pure-menu-link">release_date </a></li>
                         </ul>
                     </div>
                 </div>
                    
                 <div id="main">
-                <div class="header">
+                <div className="header">
                     <h1>Star Wars Wiki</h1>
                     <h2>A Guide to the Star Wars Film Franchise</h2>
                 </div>
 
-                    <div class="content">
+                    <div className="content">
                     { this.state.films.map((film) => (
-                        <div className="Card-body">
+                        <div className="Card-body" key={film.episode_id}>
                             <h4>Title: {film.title}</h4>
                             <h6>{film.opening_crawl}</h6>
                             <h6>Director: {film.director}</h6>
