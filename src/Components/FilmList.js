e7e1456bc596640e8546dd96d855e4ff6c840740
Arthur Arend <arthur.nox@gmail.com>
2019-09-05 11:39:25 -0300

    Troca Favicon

--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -8,8 +8,9 @@ export default class FilmList extends React.Component {
     
     componentDidMount () {
         axios.get('https://swapi.co/api/films').then(res => {
-            const films = res.data.results
-            this.setState({ films })
+            this.setState({ films: res.data.results.sort(function(a, b){
+                if (a.episode_id < b.episode_id) return -1;
+            }) })
         })
     }
 
@@ -18,26 +19,29 @@ export default class FilmList extends React.Component {
             <div id="layout">
                 <div id="menu">
                     <div class="pure-menu">
-                        <a class="pure-menu-heading" href="#">Star Wars Wiki</a>
+                        <a class="pure-menu-heading" href="#">Wars Wiki</a>
 
                         <ul class="pure-menu-list">
                             <li class="pure-menu-item"><a href="#" class="pure-menu-link">opening_crawl </a></li>
                             <li class="pure-menu-item"><a href="#" class="pure-menu-link">director </a></li>
-                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">release_date </a></li>
+                            <li class="pure-menu-item"><a href="#" value="release_date" class="pure-menu-link">release_date </a></li>
                         </ul>
                     </div>
                 </div>
                    
                 <div id="main">
-                    <header>
-                        <h1>Star Wars WIKI</h1>
-                        <h2>A guide to Star Wars Franchise</h2>
-                    </header>
+                <div class="header">
+                    <h1>Star Wars Wiki</h1>
+                    <h2>A Guide to the Star Wars Film Franchise</h2>
+                </div>
+
                     <div class="content">
-                    { this.state.films.map((film) =>(
+                    { this.state.films.map((film) => (
                         <div className="Card-body">
-                            <h5>{film.title}</h5>
-                            <h6>{film.opening_crawl }</h6>
+                            <h4>Title: {film.title}</h4>
+                            <h6>{film.opening_crawl}</h6>
+                            <h6>Director: {film.director}</h6>
+                            <h6>Release Date: {film.release_date}</h6>
                         </div>
                     ))}
 
